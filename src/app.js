bad5d1ebeabf7db87ac3296b36346820b301b9c3
Ofer Hacohen <oferhaco@gmail.com>
2020-11-21 19:11:56 +0200

    fizzbuzz flow with mocked response, handling request flows

--- src/app.js
+++ src/app.js
@@ -3,9 +3,16 @@ const app = express()
 const port = 3000;
 const NOT_ALLOWED_STATUS_CODE = 405;
 const NOT_FOUND_STATUS_CODE = 404;
+const bodyParser = require('body-parser');
+const controllers = require('./controllers');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.post('/fizzbuzz', (req, res) => {
-    res.send('Hello World!')
+    controllers.getFizzbuzz(req.body, res);
 });
 
 app.use('/fizzbuzz', (req, res) => {
---
+++ src/controllers.js
@@ -0,0 +1,11 @@
+const handleResponse = require('./shared/handleResponse').handleResponse;
+const calculateFizzbuzz = require('./logic/fizzbuzz').calculateFizzbuzz;
+
+module.exports.getFizzbuzz = (body, res) => {
+  if (!body || !body.count) {
+      return handleResponse(res, null, { error: "invalid input", status: 400 });
+  } else {
+      const fizzBuzz = calculateFizzbuzz(body.count);
+      return handleResponse(res, fizzBuzz);
+  }
+};
