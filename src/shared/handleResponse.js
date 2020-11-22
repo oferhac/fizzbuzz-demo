384db4d51d7e06b4babcc8d30ec2dbb83ed47711
Ofer Hacohen <oferhaco@gmail.com>
2020-11-22 19:35:03 +0200

    error is null instead of undefined

--- src/shared/handleResponse.js
+++ src/shared/handleResponse.js
@@ -1,4 +1,4 @@
 module.exports.handleResponse = (res, response, error) => {
     const status = error ? error.status : 200;
-    res.status(status).json({ error , response: response || '' });
+    res.status(status).json({ error: error || null , response: response || '' });
 }
