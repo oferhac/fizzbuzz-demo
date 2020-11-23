53f0f0efbb4dfd71d5b3e8ffdfaad17f22b73764
Ofer Hacohen <oferhaco@gmail.com>
2020-11-23 23:05:51 +0200

    rm status from error object

--- src/controllers.js
+++ src/controllers.js
@@ -3,7 +3,7 @@ const calculateFizzbuzz = require('./logic/fizzbuzz').calculateFizzbuzz;
 
 module.exports.getFizzbuzz = (body, res) => {
   if (!body || !body.count || typeof body.count !== "number" || body.count < 1) {
-      return handleResponse(res, null, { error: "invalid input", status: 400 });
+      return handleResponse(res, null, { errorMsg: "invalid input", status: 400 });
   } else {
       const fizzBuzz = calculateFizzbuzz(body.count);
       return handleResponse(res, fizzBuzz);
